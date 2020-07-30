b558086ab09c80adea6c5880c02b3ae322cb377e
claire <a115487@gmail.com>
2020-07-30 19:12:42 +0800

    修改

---  vue.config.js
+++  vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/0724-week6/'
-    : '/'
+  publicPath: './'
 }
--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/clairechang0609/0724-week6.git master:gh-pages
+git push -f http://github.com/clairechang0609/0724-week6.git master:gh-pages
 
 cd -
